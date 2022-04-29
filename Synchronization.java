49a7fc1e3eaf6186fe0cbbac90abd212a84fd905
kajal1801 <anshikajais2001@gmail.com>
2022-04-29 22:18:19 +0530

    Synchronization in java (MultiThreading)

---
+++ Synchronization.java
@@ -0,0 +1,34 @@
+class Bus implements Runnable
+{
+    int available = 1, passenger;
+    Bus(int passenger){
+        this.passenger = passenger;
+    }
+    public synchronized void run(){
+        String name = Thread.currentThread().getName();
+        if(available >= passenger){
+            System.out.println(name + " Reserved a seat..!");
+            available = available-passenger;
+        }
+        else{
+            System.out.println("Sorry seat already occupied..");
+        }
+    }
+}
+public class Synchronization {
+    public static void main(String args[]){
+        Bus r = new Bus(1);
+
+        Thread t1 = new Thread(r);
+        Thread t2 = new Thread(r);
+        Thread t3 = new Thread(r);
+
+        t1.setName("Ram");
+        t2.setName("Shyam");
+        t3.setName("Raju");
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
