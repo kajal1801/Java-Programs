f47a43646d84c93e75512553bd3cfbe07b431ea6
kajal1801 <anshikajais2001@gmail.com>
2022-05-03 22:57:11 +0530

    Enum Practice

---
+++ Enum1.java
@@ -0,0 +1,12 @@
+// enum Beer{
+//     KF, KO, RC, FO;
+// }
+public class Enum1 {
+    enum Beer{
+        KF, KO, RC, FO;
+    }
+    public static void main(String args[]){
+        Beer b = Beer.KF;
+        System.out.println(b);
+    }
+}
